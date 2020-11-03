21c51476240a5d7166a35c7af2edb668ea4022b6
thopd-vnpay <phuctho.phuctho@gmail.com>
2020-11-03 22:01:58 +0700

    vvv

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {Day1Component} from './day1/day1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'day1', component: Day1Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
