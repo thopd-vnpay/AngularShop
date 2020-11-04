ae778add0609f42d73e8b5febdf1c03926cdede2
thopd-vnpay <phuctho.phuctho@gmail.com>
2020-11-04 23:46:35 +0700

    vssd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Day1Component } from './day1/day1.component';
 import { HeaderComponent } from './day1/header/header.component';
-import { BodyComponent } from './day1/body/body.component';
+import {BodyComponent} from './day1/body/body.component';
 import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import { ConfirmationDialogComponent } from './day1/confirmation-dialog/confirmation-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Day1Component,
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    ConfirmationDialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/day1/body/body.component.ts
+++ src/app/day1/body/body.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {MatDialog} from '@angular/material/dialog';
+import {ConfirmationDialogComponent} from '../confirmation-dialog/confirmation-dialog.component';
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
@@ -10,9 +11,20 @@ export class BodyComponent implements OnInit {
   price2: number = 5.49;
   item1: number = 1;
   item2: number = 1;
-  constructor() { }
-
+  title = 'angular-confirmation-dialog';
+  constructor(public dialog: MatDialog) {}
+    openDialog(): void {
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      width: '350px',
+      data: "Do you confirm the deletion of this data?"
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if(result) {
+        console.log('Yes clicked');
+        // DO SOMETHING
+      }
+    });
+  }
   ngOnInit(): void {
   }
-
 }
